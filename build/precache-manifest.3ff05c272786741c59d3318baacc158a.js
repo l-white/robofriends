15c8e8b7e0cc492bb6c40a6ffef1f93ac7a02d35
Laura <law7676@outlook.com>
2021-01-16 18:11:22 -0600

    Added build files

---
+++ build/precache-manifest.3ff05c272786741c59d3318baacc158a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5fc0f2507c212aacaf3ddc3e0f6ce41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81be0e9481329052a66c",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "40e19a1e7961feee894d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "81be0e9481329052a66c",
+    "url": "/static/js/2.a566d538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a566d538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e19a1e7961feee894d",
+    "url": "/static/js/main.117c0d1c.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  }
+]);
